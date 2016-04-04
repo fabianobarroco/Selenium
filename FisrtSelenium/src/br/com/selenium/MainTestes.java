69c15ae03f81b694df1b2a26854fcc6b2a82c920
fabianobarroco <fabianobarroco@gmail.com>
2016-04-04 00:12:57 -0300

    coment

--- FisrtSelenium/src/br/com/selenium/MainTestes.java
+++ FisrtSelenium/src/br/com/selenium/MainTestes.java
@@ -7,7 +7,8 @@ public class MainTestes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//comentário testes
+		
 		TestesSenai teste1 = new TestesSenai();
 		TestesGoogle teste2 = new TestesGoogle();
 		
