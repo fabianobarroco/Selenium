9778990472a4bc6260ffb3dfe01d9f54148ce439
fabianobarroco <fabianobarroco@gmail.com>
2016-04-07 21:45:47 -0300

    \m/

--- FisrtSelenium/src/Controller/MainTestes.java
+++ FisrtSelenium/src/Controller/MainTestes.java
@@ -1,7 +1,5 @@
 package Controller;
 
-import java.io.IOException;
-
 import Config.Constants;
 import Config.Email;
 import TestsCases.TestesDigilab;
@@ -14,18 +12,10 @@ public class MainTestes{
 
 		
 		TestesDigilab formulario = new TestesDigilab();
-		//TestesSenai loginSenai = new TestesSenai();
-	    //TestesGoogle google = new TestesGoogle();
-		//TestesFile file = new TestesFile();
-		//LOG += file.lerArquivo();
-	    //file.planilha();
-		//LOG += loginSenai.validarLoginSenai();
-		//LOG += loginSenai.fecharBannerSenai();
-		//LOG += google.pesquisaGoogle("Terra");
-		//formulario.open();
-		//LOG += formulario.preecherFormulario();
-		//LOG += formulario.verificarCamposObrigatorios();
-		//LOG += formulario.verificarHintFacebook();
+		formulario.open();
+		LOG += formulario.preecherFormulario();
+		LOG += formulario.verificarCamposObrigatorios();
+		LOG += formulario.verificarHintFacebook();
 		LOG += formulario.navegacaoSiteProdutos(Constants.PLAYCHANNEL);
 		LOG += formulario.navegacaoSiteProdutos(Constants.PLAYOUT);
 		
@@ -36,12 +26,15 @@ public class MainTestes{
 
 		
 	}
+	
+	
 	// http://pt.slideshare.net/vandersonsilva9615/02-treinamentodbehaveweb  demoiselle
-	// http://pt.stackoverflow.com/questions/62934/c%C3%B3digo-selenium-webdriver-para-de-executar-ap%C3%B3s-ler-planilha-com-apache-poi/62958#62958 Ler valores em Planilha
-	//http://www.guj.com.br/t/validar-um-cadastro-utilizando-selenium-webdriver/280617 formulário
-	//http://blog.caelum.com.br/organizacao-de-testes-de-aceitacao-com-pageobjects/ //Refactorando 
-	//http://pythonclub.com.br/selenium-parte-4.html#actionschains-operacoes-avancadas
-	//http://www.qualister.com.br/blog/introducao-ao-selenium-ide
-	//https://www.youtube.com/watch?v=0bHk7HCbcDQ
-	//https://github.com/demoiselle/behave/blob/master/impl/runner/webdriver/src/main/java/br/gov/frameworkdemoiselle/behave/runner/webdriver/ui/WebBase.java
+		// http://pt.stackoverflow.com/questions/62934/c%C3%B3digo-selenium-webdriver-para-de-executar-ap%C3%B3s-ler-planilha-com-apache-poi/62958#62958 Ler valores em Planilha
+		//http://www.guj.com.br/t/validar-um-cadastro-utilizando-selenium-webdriver/280617 formulário
+		//http://blog.caelum.com.br/organizacao-de-testes-de-aceitacao-com-pageobjects/ //Refactorando 
+		//http://pythonclub.com.br/selenium-parte-4.html#actionschains-operacoes-avancadas
+		//http://www.qualister.com.br/blog/introducao-ao-selenium-ide
+		//https://www.youtube.com/watch?v=0bHk7HCbcDQ
+		//https://github.com/demoiselle/behave/blob/master/impl/runner/webdriver/src/main/java/br/gov/frameworkdemoiselle/behave/runner/webdriver/ui/WebBase.java
+	
 }
